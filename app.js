49a92562dcbc5c5209d3155903b22025731aba7a
Mvisruth <visruthm123@gmail.com>
2025-11-11 10:36:59 +0530

    updated

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 let userScore = 0
 let computerScore = 0
 
-const choices = document.querySelectorAll(".cchoice")
+const choices = document.querySelectorAll(".choice")
 
 choices.forEach((choice)=>{
-    console.log(choice);
+    choice.addEventListener('click',()=>{
+        
+        const choiceId = choice.getAttribute('id')
+        console.log(choiceId);
+        
+        console.log("choice was clicked");
+        
+    })
     
 })
