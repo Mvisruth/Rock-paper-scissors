86d09725f98bbded8e8c11db5d3c71a2b241dfe0
Mvisruth <visruthm123@gmail.com>
2025-11-12 11:25:43 +0530

    update app.js file

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 let userScore = 0;
 let computerScore = 0;
+let move = 0;
 
-const msg = document.getElementById('msg')
+const msg = document.getElementById("msg");
 
-const drawGame = ()=>{
+const drawGame = () => {
   console.log("Game was Draw");
-  msg.innerText="Game was Draw.Play again"
-}
+  msg.innerText = "Game was Draw.Play again";
+};
 
-const showWinner = (userWin)=>{
-  if(userWin){
-    msg.innerHTML = "you win"
-    msg.style.background = "green"
-    document.getElementById('user-score').innerText = userScore+=1
+const showWinner = (userWin) => {
+  if (userWin) {
+    msg.innerHTML = "you win";
+    msg.style.background = "green";
+    document.getElementById("user-score").innerText = userScore += 1;
     console.log("you win");
-  }else{
-    msg.innerHTML = "Computer win"
-        msg.style.background = "red"
-    document.getElementById('computer-score').innerText = computerScore+=1
+  } else {
+    msg.innerHTML = "Computer win";
+    msg.style.background = "red";
+    document.getElementById("computer-score").innerText = computerScore += 1;
     console.log("computer win");
   }
-}
+};
 
 const choices = document.querySelectorAll(".choice");
 //Computer Choice
@@ -32,26 +33,50 @@ const genComputerChoice = () => {
 
   return options[random];
 };
+//game Over
+const gameOver = () => {
+  const choices = document.querySelectorAll(".choice");
+  //Disable all choice
+  choices.forEach((choice) => {
+    choice.style.pointerEvents = "none";
+    choice.style.opacity = "0.5";
+  });
+  // Display final result
+  if (userScore > computerScore) {
+    msg.innerText = "Game Over ! You Won The Game";
+    msg.style.backgroundColor = "green";
+  } else if (userScore < computerScore) {
+    msg.innerText = "Game Over ! Computer Won The Game";
+    msg.style.backgroundColor = "red";
+  } else {
+    msg.innerText = "Game Over! It's a Tie!";
+    msg.style.backgroundColor = "orange";
+  }
+};
 
 const playGame = (userChoice) => {
   //user choice
   console.log("user Choice = ", userChoice);
   //Genrate Computer Choice
-  const cmpChoice = genComputerChoice()
+  const cmpChoice = genComputerChoice();
   console.log("computer Choice = ", cmpChoice);
-
-  if(userChoice===cmpChoice){
-    drawGame()
-  }else{
-    let userWin = true
-    if(userChoice=="rock"){
-      userWin = cmpChoice =="paper"?false:true
-    }else if(userChoice=="paper"){
-      userWin = cmpChoice =="scissors"?false:true
-    }else{
-      userWin = cmpChoice =="rock"?false:true
+  //increment move
+  move++;
+  if (userChoice === cmpChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice == "rock") {
+      userWin = cmpChoice == "paper" ? false : true;
+    } else if (userChoice == "paper") {
+      userWin = cmpChoice == "scissors" ? false : true;
+    } else {
+      userWin = cmpChoice == "rock" ? false : true;
     }
-    showWinner(userWin)
+    showWinner(userWin);
+  }
+  if (move >= 10) {
+    gameOver();
   }
 };
 
